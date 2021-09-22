4dd1186450ecd890b223394853ede269233bda70
jamesee <james.ee.developer@gmail.com>
2021-09-23 00:14:58 +0800

    revert modified publishEmail in amqp.js

--- src/services/amqp.js
+++ src/services/amqp.js
@@ -18,7 +18,7 @@ module.exports = () => {
     await channel.close()
     await client.close()
   }
-  
+
   // service.publishEmail = async (email, role, todo_id) => {
   //   const client = await amqplib.connect(URL)
   //   const channel = await client.createChannel()
@@ -26,31 +26,49 @@ module.exports = () => {
   //   const result = await channel.sendToQueue(ACCESSCONTROL_QUEUE, Buffer.from(JSON.stringify({email, role, todo_id})), {
   //     contentType: 'application/json'})
   //   // console.log(result)
-    
+
   //   await channel.close()
   //   await client.close()
 
   //   return result
   // }
 
+  // service.publishEmail = async (email, role, todo_id) => {
+  //   const open = amqplib.connect();
+  //   return await open
+  //   .then(async function(c) {
+  //      return await c.createConfirmChannel().then(async function(ch) {
+  //       return await ch.sendToQueue(ACCESSCONTROL_QUEUE, Buffer.from(JSON.stringify({email, role, todo_id})), {},
+  //         function(err, ok) {
+  //           if (err !== null){
+  //             console.warn('Message nacked!');
+  //             return 'Message nacked!'
+  //           } else {
+  //             console.log('Message acked');
+  //             return 'Message acked'
+  //           }
+  //         }
+  //       );
+  //     })
+  //   });    
+  // }
+
   service.publishEmail = async (email, role, todo_id) => {
     const open = amqplib.connect();
-    return await open
-    .then(async function(c) {
-       return await c.createConfirmChannel().then(async function(ch) {
-        return await ch.sendToQueue(ACCESSCONTROL_QUEUE, Buffer.from(JSON.stringify({email, role, todo_id})), {},
-          function(err, ok) {
-            if (err !== null){
-              console.warn('Message nacked!');
-              return 'Message nacked!'
-            } else {
-              console.log('Message acked');
-              return 'Message acked'
+    open
+      .then(async function (c) {
+        c.createConfirmChannel().then(async function (ch) {
+          ch.sendToQueue(ACCESSCONTROL_QUEUE, Buffer.from(JSON.stringify({ email, role, todo_id })), {},
+            function (err, ok) {
+              if (err !== null) {
+                console.warn('Message nacked!');
+              } else {
+                console.log('Message acked');
+              }
             }
-          }
-        );
-      })
-    });    
+          );
+        })
+      });
   }
 
   return service
